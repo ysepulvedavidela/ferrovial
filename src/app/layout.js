8ff279dbd1f80480de9c9da52ca3c352c5e91325
Yeremy <yeremy.sepulveda@gmail.com>
2024-03-07 09:45:59 -0300

    save

---
+++ src/app/layout.js
@@ -0,0 +1,2 @@
+const layout =
+'<div class="main"><div class="video-background"><iframe class="videobackground" src="https://www.youtube.com/embed/Al0Cpqw4O5E?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=Al0Cpqw4O5E" title="YouTube video player" frameborder="0" allowfullscreen></iframe></div></div>'
---
+++ src/app/link.js
@@ -0,0 +1,2 @@
+const link =
+'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><link rel="stylesheet" href="src/css/style.css"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><script src="https://kit.fontawesome.com/792df3d6b7.js" crossorigin="anonymous"></script>'
